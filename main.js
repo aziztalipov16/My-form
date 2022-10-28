b9fe07f729a853a005855664f0ac8f0dfc7fe7ce
aziztalipov16 <109068745+aziztalipov16@users.noreply.github.com>
2022-10-28 16:59:22 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+let nameInp = document.querySelector('#name');
+let emailInp = document.querySelector('#email');
+let textarea = document.querySelector('#message');
+let warn = document.querySelector('#warn');
+let btn = document.querySelector('#btn-check');
+
+function checkData() {
+	let name = nameInp.value;
+	let email = emailInp.value;
+	let message = textarea.value;
+	let check = /.+?@(mail\.ru|gmail\.com)/g.test(email);
+
+	if (!name && !check) {
+		warn.style.display = 'block';
+		warn.textContent = 'Неправильно введены данные: Имя, Email';
+		return;
+	}
+	if (!name) {
+		warn.style.display = 'block';
+		warn.textContent = 'Неправильно введены данные: Имя';
+		return;
+	}
+	if (!message) {
+		warn.style.display = 'block';
+		warn.textContent = 'Вы не ввели сообщение';
+		return;
+	}
+	if (!check) {
+		warn.style.display = 'block';
+		warn.textContent = 'Неправильно введены данные: Email';
+		return;
+	}
+
+	if (name && check && message) {
+		warn.style.display = 'none';
+		alert('Успешно отправлено!');
+	}
+}
+btn.addEventListener('click', checkData);
